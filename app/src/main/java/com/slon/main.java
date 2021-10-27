1521299fde3ce5aff9f8d2d5bfdad5d702ca370b
selimbr <59741733+selimbr@users.noreply.github.com>
2021-10-27 20:41:55 +0200

    Update main.java

--- app/src/main/java/com/slon/main.java
+++ app/src/main/java/com/slon/main.java
@@ -3,7 +3,7 @@ package com.slon;
 public class main {
     public static void main(String[] args) {
 
-        System.out.println("KRISH LE BOSS");
+        System.out.println("slon LE BOSS");
 
     }
 }
